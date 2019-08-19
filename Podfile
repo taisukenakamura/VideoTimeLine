8dedb3e2b7262b9a98e7e82b66ab47f078aab3a7
中村泰輔 <>
2019-08-19 13:17:40 +0900

    ログインページレイアウト

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '10.0'
+
+target 'VideoTimeLine' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for VideoTimeLine
+
+  target 'VideoTimeLineTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'VideoTimeLineUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- VideoTimeLine/ViewController.swift
+++ VideoTimeLine/ViewController.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Bundle Resourcesからsample.mp4を読み込んで再生
+        let path = Bundle.main.path(forResource: "System - 697", ofType: "mp4")!
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        player.play()
+        
+        // AVPlayer用のLayerを生成
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = view.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        playerLayer.zPosition = -1 // ボタン等よりも後ろに表示
+        view.layer.insertSublayer(playerLayer, at: 0) // 動画をレイヤーとして追加
+        
+        // 動画の上に重ねる半透明の黒いレイヤー
+        let dimOverlay = CALayer()
+        dimOverlay.frame = view.bounds
+        dimOverlay.backgroundColor = UIColor.black.cgColor
+        dimOverlay.zPosition = -1
+        dimOverlay.opacity = 0.6 // 不透明度
+        view.layer.insertSublayer(dimOverlay, at: 0)
+
     }
-
-
+    
 }
 
