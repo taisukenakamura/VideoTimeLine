ee7838e966bc9e87aebed128c5216bfbd9a971fa
中村泰輔 <>
2019-08-19 12:35:58 +0900

    first commit

---
+++ VideoTimeLine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoTimeLine
+//
+//  Created by 中村泰輔 on 2019/08/19.
+//  Copyright © 2019 icannot.t.n. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
